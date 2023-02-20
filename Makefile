5824d89e0fdb80449e5a09225c314fb7a95e0ac9
crazyoptimist <hey@crazyoptimist.net>
2023-02-20 15:21:22 -0600

    feat: add migration script

--- Makefile
+++ Makefile
@@ -1,2 +1,4 @@
+db_migrate:
+	go run ./cmd/api/migration/main.go
 build_linux:
 	CGO_ENABLED=0 GOOS=linux go build -o ./bin/ ./...
---
+++ cmd/api/migration/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"gin-starter/cmd/api/config"
+	"gin-starter/internal/user"
+)
+
+func main() {
+	if err := config.LoadConfig("./configs"); err != nil {
+		panic(fmt.Errorf("invalid application configuration: %s", err))
+	}
+	config.ConnectDB()
+	config.Config.DB.AutoMigrate(&user.User{})
+}
