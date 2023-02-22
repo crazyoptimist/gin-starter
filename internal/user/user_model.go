e40649f34918cf61ba9e6715fdb1a798db13c908
crazyoptimist <hey@crazyoptimist.net>
2023-02-22 13:40:41 -0600

    nit: replace snake_case with camelCase

--- internal/user/user_model.go
+++ internal/user/user_model.go
@@ -4,8 +4,9 @@ import "gin-starter/pkg/common"
 
 type User struct {
 	common.Model
-	FirstName string `gorm:"column:first_name" json:"first_name"`
-	LastName  string `gorm:"column:last_name" json:"last_name"`
+	FirstName string `gorm:"column:first_name" json:"firstName"`
+	LastName  string `gorm:"column:last_name" json:"lastName"`
 	Address   string `gorm:"column:address" json:"address"`
 	Email     string `gorm:"column:email" json:"email"`
+	Password  string `gorm:"column:password" json:"-"`
 }
