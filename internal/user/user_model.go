d3efc8e1f04e615ddd2b8904f639273316f3ac95
crazyoptimist <hey@crazyoptimist.net>
2023-02-24 02:45:52 -0600

    feat: add mapper func

--- internal/user/user_model.go
+++ internal/user/user_model.go
@@ -9,3 +9,42 @@ type User struct {
 	Email     string `gorm:"column:email" json:"email"`
 	Password  string `gorm:"column:password" json:"-"`
 }
+
+type CreateUserDto struct {
+	FirstName string `gorm:"column:first_name" json:"firstName"`
+	LastName  string `gorm:"column:last_name" json:"lastName"`
+	Email     string `gorm:"column:email" json:"email"`
+}
+
+func MapCreateUserDto(dto CreateUserDto) User {
+	// TODO: generate a random passoword
+	return User{
+		FirstName: dto.FirstName,
+		LastName:  dto.LastName,
+		Email:     dto.Email,
+		Password:  "you**must**reset**it**at**the**first**login",
+	}
+}
+
+type LoginDto struct {
+	Email    string `json:"email" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+
+type RegisterDto struct {
+	FirstName            string `json:"firstName"`
+	LastName             string `json:"lastName"`
+	Email                string `json:"email" binding:"required"`
+	Password             string `json:"password" binding:"required"`
+	PasswordConfirmation string `json:"passwordConfirmation" binding:"required, eq|eqfield=Password"`
+}
+
+func MapRegisterDto(dto RegisterDto) User {
+	// TODO: generate a random passoword
+	return User{
+		FirstName: dto.FirstName,
+		LastName:  dto.LastName,
+		Email:     dto.Email,
+		Password:  dto.Password,
+	}
+}
