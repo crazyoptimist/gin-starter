b23f1d7b62c847589c36a727224446a18267d47e
crazyoptimist <hey@crazyoptimist.net>
2023-02-28 11:04:37 -0600

    feat: graceful shutdown

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
 	"gin-starter/internal/core/config"
 	"gin-starter/internal/core/router"
@@ -15,7 +20,7 @@ import (
 
 func main() {
 	if err := config.LoadConfig(".env"); err != nil {
-		fmt.Println("Warning: dotenv file is missing, please make sure you have configured environment variables properly", err)
+		log.Println("Warning: dotenv file is missing, please make sure you have configured environment variables properly", err)
 	}
 
 	config.ConnectDB()
@@ -23,5 +28,27 @@ func main() {
 	r := router.RegisterRoutes()
 	router.SetupSwagger(r)
 
-	log.Fatalln(r.Run(fmt.Sprintf(":%v", config.Config.ServerPort)))
+	server := &http.Server{
+		Addr:    fmt.Sprintf(":%v", config.Config.ServerPort),
+		Handler: r,
+	}
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil {
+			log.Println("ListenAndServe: ", err)
+		}
+	}()
+
+	// Graceful shutdown
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	log.Println("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+	log.Println("Server exiting")
 }
