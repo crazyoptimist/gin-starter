38ad8482db79dd2dc08b2f15eae04c7db3e8ee98
crazyoptimist <hey@crazyoptimist.net>
2023-02-27 18:47:38 -0600

    fix: read config in test

--- internal/auth/jwt_helper_test.go
+++ internal/auth/jwt_helper_test.go
@@ -4,10 +4,18 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+
+	"gin-starter/cmd/api/config"
 )
 
 func TestGenerateAccessToken(t *testing.T) {
+
+	if err := config.LoadConfig("../../test.env"); err != nil {
+		panic("Failed to load test env file")
+	}
+
 	t.Run("it should generate a valid access token", func(t *testing.T) {
+
 		userId := uint(1)
 		accessToken, err := GenerateAccessToken(userId)
 		assert.NoError(t, err)
